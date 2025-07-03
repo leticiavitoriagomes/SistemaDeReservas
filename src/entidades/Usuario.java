c961d6c96cb7bcce7335c872db4361320f945220
leticiavitoriagomes <gomesleticiav@gmail.com>
2025-07-03 17:23:29 -0300

    Criando a classe Usuario

---
+++ src/entidades/Usuario.java
@@ -0,0 +1,9 @@
+package entidades;
+
+public abstract class Usuario {
+
+    protected String nomeCompleto;
+    protected String emailInstitucinal;
+    protected String telefone;
+    protected String senha;
+}
