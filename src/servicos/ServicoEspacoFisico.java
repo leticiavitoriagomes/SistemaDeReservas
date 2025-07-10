168373c76ad999050b7099ebf14f7f72da2cfb5f
leticiavitoriagomes <gomesleticiav@gmail.com>
2025-07-09 23:35:25 -0300

    pequena correção

--- src/servicos/ServicoEspacoFisico.java
+++ src/servicos/ServicoEspacoFisico.java
@@ -1,21 +1,21 @@
-package servicos;
+    package servicos;
 
-import entidades.EspacoFisico;
-import java.util.ArrayList;
-import java.util.List;
+    import entidades.EspacoFisico;
+    import java.util.ArrayList;
+    import java.util.List;
 
-public class ServicoEspacoFisico {
-    private List<EspacoFisico> espacos = new ArrayList<>();
+    public class ServicoEspacoFisico {
+        private List<EspacoFisico> espacos = new ArrayList<>();
 
-    public void adicionar(EspacoFisico e) {
-        espacos.add(e);
+        public void adicionar(EspacoFisico e) {
+            espacos.add(e);
+        }
+
+        public List<EspacoFisico> listar() {
+            return espacos;
+        }
+
+        public void remover(String local) {
+            espacos.removeIf(e -> e.getLocalizacao().equalsIgnoreCase(local.trim()));
+        }
     }
-
-    public List<EspacoFisico> listar() {
-        return espacos;
-    }
-
-    public void remover(String local) {
-        espacos.removeIf(e -> e.getLocalizacao().equalsIgnoreCase(local.trim()));
-    }
-}
